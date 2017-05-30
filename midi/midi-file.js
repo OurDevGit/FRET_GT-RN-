8efbcadb4b584ccda57b78e4b30d45f0e43a3bce
Stephen Haase <steve@bigswing.com>
2017-05-30 12:02:56 -0400

    fix to notes on and off events

--- midi/midi-file.js
+++ midi/midi-file.js
@@ -5,11 +5,6 @@ const MidiNoteTrack = require("./midi-note-track")
 const MidiPatternTrack = require("./midi-pattern-track")
 
 const parse = (filename) => {
-  // ✔︎ time/beats
-  // ✔︎ tracks
-  // ✔︎ markers
-  // ✔︎ patterns / jambar
-  // ✔︎ tuning
   // notes
   // rpn
 
@@ -20,60 +15,40 @@ const parse = (filename) => {
   var timingTrack = new MidiTimingTrack(midi.tracks[0], midi.header)
   var markers = timingTrack.markers
   var patterns = []
-  //console.log("MARKERS: ")
-  //console.log(markers)
-  //console.log(" ")
+  var tuningTracks = []
+  var guitarTracks = []
+  // console.log("MARKERS: ")
+  // console.log(markers)
+  // console.log(" ")
 
   midi.tracks.forEach(track => {
     if (track[0].text !== undefined) {
 
-      if (track[0].text.includes("FMP -") && track[0].text !== "FMP - Jam Bar") {
-        
+      if (track[0].text.includes("FMP -") && track[0].text !== "FMP - Jam Bar" && track[0].text === "FMP - Rhythm Guitar (Tune 1/2 Step Flat)") {
+        var guitarTrack = new MidiNoteTrack(track, timingTrack.secondsForTicks)
+        console.log("Guitar: ", guitarTrack.name)
+        console.log(guitarTrack.notes)
+        console.log(" ")
+        guitarTracks.push(guitarTrack)
       }
 
       if (track[0].text.includes("T -")) {
         var tuningTrack = new MidiNoteTrack(track, timingTrack.secondsForTicks)
-        console.log("TUNING: ", tuningTrack.name)
-        console.log(tuningTrack.notes)
-        console.log(" ")
+        // console.log("TUNING: ", tuningTrack.name)
+        // console.log(tuningTrack.notes)
+        // console.log(" ")
+        tuningTracks.push(tuningTrack)
       }
 
       if (track[0].text.includes("FMP - Jam Bar")) {
         var patternTrack = new MidiPatternTrack(track, timingTrack.secondsForTicks)
         patterns = patternTrack.patterns
-        console.log("JAM BAR: ")
-        console.log(patterns)
-        console.log(" ")
+        // console.log("JAM BAR: ")
+        // console.log(patterns)
+        // console.log(" ")
       }
     }
   })
-  
-  midi.tracks.forEach(track => {
-    track.forEach(event => {
-      if (event.subtype !== "noteOn" && event.subtype !== "noteOff") {
-        // console.log(event.type, event.subtype);
-
-        if (event.subtype === "controller") {
-          /*console.log({
-            type: event.controllerType,
-            track: event.value
-          });*/
-        }
-
-        /*
-        if (event.subtype === "controller") {
-          console.log({
-            type: event.controllerType,
-            val: event.value
-          });
-        }*/
-      }
-    });
-  });
-
-  // console.log(JSON.stringify(midi, null, 2));
 };
-
-
 
 parse("../walk_away");
--- midi/midi-note-track.js
+++ midi/midi-note-track.js
@@ -13,7 +13,9 @@ function MidiNoteTrack(track, secondsForTicks) {
       if (event.text !== undefined) {
         if (event.text.includes("T - ")) {
           this.name = event.text.replace("T - ", "")
-        }
+        } else if (event.text.includes("FMP - ")) {
+          this.name = event.text.replace("T - ", "")
+        } 
       }
 
       if (event.deltaTime !== undefined) {
@@ -21,26 +23,31 @@ function MidiNoteTrack(track, secondsForTicks) {
       }
       
       if (event.subtype === "noteOn") {
-          if (event.velocity > 0) {
-            event.begin = secondsForTicks(totalTicks)
-            notesOn.push(event)
-          }
-        } else if (event.subtype === "noteOff") {
-          for (var i = 0; i < notesOn.length; i++) {
-            var noteOn = notesOn[i]
+        if (event.velocity > 0) {
+          event.begin = secondsForTicks(totalTicks)
+          notesOn.push(event)
+        }
+      } else if (event.subtype === "noteOff") {
+        for (var i = 0; i < notesOn.length; i++) {
+          var noteOn = notesOn[i]
 
-            if (event.channel === noteOn.channel && event.noteNumber == noteOn.noteNumber) {
-              var note = {}
-              note.string = noteOn.channel - 10
-              note.fret = noteOn.noteNumber - stringOffset[note.string]
-              note.begin = noteOn.begin
-              note.end = secondsForTicks(totalTicks)
+          if (event.channel === noteOn.channel && event.noteNumber == noteOn.noteNumber) {
+            var note = {}
+            note.string = noteOn.channel - 10
+            note.fret = noteOn.noteNumber - stringOffset[note.string]
+            note.begin = noteOn.begin
+            note.end = secondsForTicks(totalTicks)
 
-              this.notes.push(note)
-              notesOn.splice(i, 0)
-              break
-            }
+            this.notes.push(note)
+            notesOn.splice(i, 1)
+            break
           }
+        }
+      } else if (event.subtype === "controller") {
+          /*console.log({
+            type: event.controllerType,
+            track: event.value
+          });*/
         }
     });
 }
