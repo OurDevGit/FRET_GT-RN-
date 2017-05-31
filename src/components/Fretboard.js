0b03cde797e9ad042eb47ce23fbe48848295de32
Rick Terrill <rick@bigswing.com>
2017-05-31 10:48:44 -0400

    going to start on Fretboard

---
+++ src/components/Fretboard.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View, Image } from "react-native";
+
+const fb = require("../fretboard.png");
+
+const Fretboard = () => <View><Image source={fb} /></View>;
+
+export default Fretboard;
