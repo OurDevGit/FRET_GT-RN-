4e7ce1fc0d2af893c8e2ae9bebbbf44c91937c38
Stephen Haase <steve@bigswing.com>
2017-07-11 11:08:30 -0400

    added gradient to nut

--- android/app/src/main/java/com/guitartunes/MainApplication.java
+++ android/app/src/main/java/com/guitartunes/MainApplication.java
@@ -3,6 +3,7 @@ package com.guitartunes;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.zmxv.RNSound.RNSoundPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LinearGradientPackage(),
             new RNSoundPackage(),
             new RNFetchBlobPackage()
       );
--- src/components/Fretboards/FretboardFret.js
+++ src/components/Fretboards/FretboardFret.js
@@ -1,17 +1,13 @@
 import React from "react";
 import { View, Text } from "react-native";
+import LinearGradient from 'react-native-linear-gradient';
 import FretboardNote from "./FretboardNote";
 import FretMarkers from "./FretboardMarkers";
 
-
 const bar = { position: "absolute", right: 0, width: 4, height: "100%", backgroundColor: '#CCCCCC' }
 
-const backgroundStyle = fretIndex => {
-  if (fretIndex === 0) {
-    return { position: "absolute", width: "100%", height: "100%", backgroundColor: "#b17c2f" }
-  } else {
-    return { position: "absolute", width: "100%", height: "100%", backgroundColor: 'black' }
-  }
+const backgroundColor = fretIndex => {
+  return fretIndex > 0 ? ["black", "black"] : ["#f2b172", "#f0b072", "#c29b74", "#8d7b68", "#4f4c48"]
 }
 
 const fretName = index => {
@@ -33,7 +29,12 @@ const FretboardFret = ({ index, track, isBass, style }) => (
   <View style={{ ...style, flex: 1 }}>
     <Text style={{ width: "100%", height: 12, fontSize: 10, marginBottom: 4, textAlign: "center", backgroundColor: "#E6D9B9" }} >{fretName(index)}</Text>
     <View style={{ flex: 1}}>
-      <View style={backgroundStyle(index)} />
+      <LinearGradient 
+        colors={backgroundColor(index)} 
+        start={{ x: 0.1, y: 0.0 }}
+        end={{ x: 0.9, y: 0.0 }}
+        style={{ position: "absolute", width: "100%", height: "100%" }} 
+      />
       <View style={bar} />
       <FretMarkers fretIndex={index}/>
       
