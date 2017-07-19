d6f3c5cb8a5daaefde9b17998a50e94461e44c35
Stephen Haase <steve@bigswing.com>
2017-07-19 15:28:42 -0400

    fix to bass note strings

--- src/components/Fretboards/FretboardFret.js
+++ src/components/Fretboards/FretboardFret.js
@@ -34,10 +34,10 @@ const FretboardFret = ({ track, fret, isBass, style }) => (
     <Text style={{ width: "100%", height: 12, fontSize: 10, marginBottom: 4, textAlign: "center", backgroundColor: "#E6D9B9" }} >{fret > 0 ? fret : "Nut"}</Text>
     <View style={{ flex: 1}}>
       <View style={{ width: "100%", height: "100%", paddingVertical: 3, justifyContent: "space-between", alignItems: "center", marginLeft: -3 }} >
-        <FretboardNote key={0} track={track} fret={fret} string={0} notation={notation(fret, 0)}/>
-        <FretboardNote key={1} track={track} fret={fret} string={1} notation={notation(fret, 1)}/>
-        <FretboardNote key={2} track={track} fret={fret} string={2} notation={notation(fret, 2)}/>
-        <FretboardNote key={3} track={track} fret={fret} string={3} notation={notation(fret, 3)}/>
+        <FretboardNote key={0} track={track} fret={fret} string={isBass ? 2 : 0} notation={notation(fret, 0)}/>
+        <FretboardNote key={1} track={track} fret={fret} string={isBass ? 3 : 1} notation={notation(fret, 1)}/>
+        <FretboardNote key={2} track={track} fret={fret} string={isBass ? 4 : 2} notation={notation(fret, 2)}/>
+        <FretboardNote key={3} track={track} fret={fret} string={isBass ? 5 : 3} notation={notation(fret, 3)}/>
 
         {!isBass &&  <FretboardNote key={4} track={track} fret={fret} string={4} notation={notation(fret, 4)} />}
         {!isBass &&  <FretboardNote key={5} track={track} fret={fret} string={5} notation={notation(fret, 5)}/>}
