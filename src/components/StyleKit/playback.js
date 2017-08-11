7f175db4a97229682fd573395dac7574e1b6e831
Rick Terrill <rick@bigswing.com>
2017-08-11 16:12:48 -0400

    reorging StyleKit stuff

--- src/components/StyleKit/playback.js
+++ src/components/StyleKit/playback.js
@@ -17,3 +17,28 @@ export const BtnPlay = props => {
     />
   );
 };
+
+export const BtnBuy = ({ text, fontSize, width, height }) => {
+  return (
+    <PaintCode
+      // drawBuyButton(Canvas canvas, Context context, String priceText, float fontSize, String topText, String bottomText)
+      drawMethod="BuyButton"
+      drawArgs={["priceText", "fontSize", "topText", "bottomText"]}
+      priceText="$1.99"
+      fontSize={12}
+      topText=""
+      bottomText=""
+      style={{ width: 50, height: 25 }}
+    />
+  );
+};
+
+export const Heart = ({ isPressed }) => {
+  return (
+    <PaintCode
+      drawMethod="BtnFavorite"
+      isPressed={isPressed}
+      style={{ width: 20, height: 20 }}
+    />
+  );
+};
