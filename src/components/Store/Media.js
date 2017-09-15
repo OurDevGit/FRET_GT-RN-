e8e26c8e78055fc2dcdc1e477057fee695262e3c
Rick Terrill <rick@bigswing.com>
2017-09-15 17:43:23 -0400

    starting on search with Fuse

--- src/components/Store/Media.js
+++ src/components/Store/Media.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { View, TextInput, TouchableOpacity, Text, Button } from "react-native";
 
+import Fuse from "fuse.js";
+import _ from "lodash";
+
 import { PrimaryGold } from "../../design";
 import { FlatButton } from "../Material";
 import TabbedMedia from "./TabbedMedia";
 
-const CloseButton = ({ title }) => (
-  <TouchableOpacity onPress={() => console.log("touch")}>
-    <View style={{ padding: 8, marginRight: 8 }}>
-      <Text style={{ color: PrimaryGold }}>{title.toUpperCase()}</Text>
-    </View>
-  </TouchableOpacity>
-);
+const fuseOptions = {
+  shouldSort: true,
+  threshold: 0.6,
+  location: 0,
+  distance: 100,
+  maxPatternLength: 32,
+  minMatchCharLength: 1,
+  keys: ["title", "artist"]
+};
 
 class Media extends React.PureComponent {
   state = {
@@ -35,11 +40,15 @@ class Media extends React.PureComponent {
               flexGrow: 1,
               marginRight: 8
             }}
-            onChangeText={searchText => this.setState({ searchText })}
+            onChangeText={this.handleChangeText}
             value={this.state.searchText}
           />
           <Text>{this.props.media.length}</Text>
-          <FlatButton title="Close" style={{ color: PrimaryGold }} />
+          <FlatButton
+            title="Close"
+            style={{ color: PrimaryGold }}
+            onPress={() => null}
+          />
         </View>
 
         <TabbedMedia
@@ -50,6 +59,26 @@ class Media extends React.PureComponent {
       </View>
     );
   }
+
+  componentWillReceiveProps(nextProps) {
+    // console.log("Media next props");
+    // console.log(nextProps.media.length);
+    // console.log(nextProps.media[0].data.length);
+
+    const allMedia = _.flatMap(nextProps.media, m => _.toArray(m.data));
+    // console.debug(allMedia[0]);
+    this.fuse = new Fuse(allMedia, fuseOptions); // "list" is the item array
+  }
+
+  handleChangeText = text => {
+    if (this.fuse) {
+      const result = this.fuse.search(text);
+
+      console.debug(text);
+      console.debug({ searchResult: result.length });
+    }
+    this.setState({ searchText: text });
+  };
 }
 
 export default Media;
