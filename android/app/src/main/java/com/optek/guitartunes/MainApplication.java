dc3ee26c3ad6c7ca8dbbb641628a3efefe30df3a
Stephen Haase <steve@bigswing.com>
2017-10-06 11:00:13 -0500

    using exoplayer!
    It appears that seeking works much better now and tempo is still working

--- android/app/src/main/java/com/optek/guitartunes/MainApplication.java
+++ android/app/src/main/java/com/optek/guitartunes/MainApplication.java
@@ -3,6 +3,7 @@ package com.optek.guitartunes;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.brentvatne.react.ReactVideoPackage;
 import com.tavernari.volumecontroller.ReactNativeVolumeControllerPackage;
 import com.idehub.Billing.InAppBillingBridgePackage;
 import com.brentvatne.react.ReactVideoPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(new MainReactPackage(),
+            new ReactVideoPackage(),
             new ReactNativeVolumeControllerPackage(),
             new InAppBillingBridgePackage(), new ReactVideoPackage(),
           new RNSoundPackage(), new RNFetchBlobPackage(), new BSTestViewReactPackage());
