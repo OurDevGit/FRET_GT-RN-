7aab23effe209ed980d8224efd93575964a1cdeb
Rick Terrill <rick@bigswing.com>
2017-10-26 10:05:44 -0400

    added SET_CURRENT_MEDIA to the currentLoop reducer

--- src/redux/reducers/loop.js
+++ src/redux/reducers/loop.js
@@ -4,7 +4,11 @@ exports.currentLoop = (state = Map(), action) => {
   switch (action.type) {
     case "SET_CURRENT_LOOP":
       return action.payload;
-    case "CLEAR_CURRENT_LOOP" || "CLEAR_MIDI_DATA":
+    case "CLEAR_CURRENT_LOOP":
+      return Map();
+    case "CLEAR_MIDI_DATA":
+      return Map();
+    case "SET_CURRENT_MEDIA":
       return Map();
     default:
       return state;
