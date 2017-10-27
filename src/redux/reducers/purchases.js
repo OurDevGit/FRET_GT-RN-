e8b4c6096bfd90dc753597af530d2bc8afe1223e
Rick Terrill <rick@bigswing.com>
2017-10-27 13:46:38 -0400

    fix for media not showing up in Purchased tab after a purchase

--- src/redux/reducers/purchases.js
+++ src/redux/reducers/purchases.js
@@ -2,8 +2,9 @@ import { Set } from "immutable";
 
 export const purchasedMedia = (state = Set(), action) => {
   switch (action.type) {
-    case "ADD_PURCHASED_MEDIA":
-      return state.add(action.payload);
+    case "ADD_PURCHASED_MEDIA": {
+      return state.add(action.payload.toLowerCase());
+    }
     case "SET_PURCHASED_MEDIA":
       return Set(action.payload);
     default:
