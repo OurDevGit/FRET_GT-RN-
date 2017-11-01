bb82baf9572a4a3284ca55d8a82ae59998838824
Stephen Haase <steve@bigswing.com>
2017-11-01 13:15:48 -0600

    moved scanning start into specific action types

--- android/app/src/main/java/com/optek/guitartunes/ble/GTGuitarController.java
+++ android/app/src/main/java/com/optek/guitartunes/ble/GTGuitarController.java
@@ -37,10 +37,11 @@ public class GTGuitarController extends ReactContextBaseJavaModule {
       public void onChange(String action, String guitarId) {
         if (action == "connect") {
           guitarEmitter.emit("GUITAR_CONNECTED", guitarId);
+          startScanning();
         } else {
           guitarEmitter.emit("GUITAR_DISCONNECTED", guitarId);
+          startScanning();
         }
-        startScanning();
       }
     });
   }
