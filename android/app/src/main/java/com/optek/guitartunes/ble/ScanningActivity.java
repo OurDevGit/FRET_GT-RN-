fc77a82ce4fff1013677bfe0337eca67a90591af
Stephen Haase <steve@bigswing.com>
2017-12-19 21:28:07 -0700

    working on layout for activity

--- android/app/src/main/java/com/optek/guitartunes/ble/ScanningActivity.java
+++ android/app/src/main/java/com/optek/guitartunes/ble/ScanningActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.optek.fretlight.sdk.Fretlight;
 import com.optek.fretlight.sdk.FretlightClient;
@@ -25,10 +27,15 @@ public class ScanningActivity extends Activity {
     Sentry.record(new BreadcrumbBuilder().setMessage(message).build());
   }
 
+  public void onDoneClick(final View view) {
+    Log.d("GTGuitarController", "Done");
+  }
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     Log.d("GTGuitarController", "onCreate()");
     super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_scanner);
 
     mGuitars = Guitars.getInstance();
     guitarEmitter = GuitarEmitter.getInstance();
