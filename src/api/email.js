c63608e95fd821526622bfb621ae49fc5a6ae8b5
Stephen Haase <steve@bigswing.com>
2018-03-06 07:42:34 -0700

    updates to email service calls

--- src/api/email.js
+++ src/api/email.js
@@ -1,14 +1,14 @@
 const baseURL = "https://h59y6nr7la.execute-api.us-east-1.amazonaws.com/PROD/"; //\(stage)/\(resource)
 
 export const registerEmail = email => {
-  const url = `${baseURL}user/register?email=${email}&listId=1362267580`;
+  const url = `${baseURL}user/register?email=${email}&type=updates`;
   return fetch(url).then(response => {
     return response.json();
   });
 };
 
 export const registerContestEmail = email => {
-  const url = `${baseURL}user/register?email=${email}&listId=1095395097`;
+  const url = `${baseURL}user/register?email=${email}&type=contest`;
   return fetch(url).then(response => {
     return response.json();
   });
